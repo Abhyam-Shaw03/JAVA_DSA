fd275a58890671c84642462016aeff6a8af52c68
Abhyam Shaw <abhyamshaw3@gmail.com>
2025-04-30 00:14:41 +0530

    Created and pushed Binary Search folder with 4 programs on 1d Array

---
+++ src/binarySearch/Arrays_1D/simpleBinarySearch.java
@@ -0,0 +1,25 @@
+/*
+    Given a sorted array of integers nums with 0-based indexing, find the index of a specified target integer. If the target is found in the array, return its index. If the target is not found, return -1.
+*/
+
+package binarySearch.Arrays_1D;
+
+public class simpleBinarySearch {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0, high = n-1;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] > target){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/binarySearch/Arrays_1D/upperBound.java
@@ -0,0 +1,38 @@
+/*
+       Given a sorted array of nums and an integer x, write a program to find the upper bound of x. The upper bound algorithm finds the first or the smallest index in a sorted array where the value at that index is greater than a given key i.e. x.
+
+    If no such index is found, return the size of the array.
+
+
+    Examples:
+    Input : n= 4, nums = [1,2,2,3], x = 2
+
+    Output:3
+
+    Explanation: Index 3 is the smallest index such that arr[3] > x.
+
+    Input : n = 5, nums = [3,5,8,15,19], x = 9
+
+    Output: 3
+
+    Explanation: Index 3 is the smallest index such that arr[3] > x.
+ */
+
+package binarySearch.Arrays_1D;
+
+public class upperBound {
+    public int upperBound(int[] nums, int x) {
+        int n = nums.length;
+        int low = 0, high = n-1, ans = n;
+        while(low <= high){
+            int mid = (low + high)/2;
+            if(nums[mid] > x){
+                ans = mid;
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
